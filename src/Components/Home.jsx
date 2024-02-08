aedf0df7dbdd387b3e99eccbc0404a1e7cffc74a
josh_schilling <josh_schilling@aol.com>
2024-02-08 12:09:34 -0500

    Routes and Pages

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our BookStore</h1>
+      <h2>Amazons Top Competitor!</h2>
+      <p>Feel free to look around for your favorite books</p>
+    </div>
+  );
+}
+
+export default Home;
